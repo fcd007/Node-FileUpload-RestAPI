4580573ebf7c7441c346e43ca7a629d65cc0a1e3
Claudeilton Dantas <fcd007@hotmail.com>
2022-02-03 23:57:15 -0300

    feat: configuracao upload files server

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -1,4 +1,6 @@
-const uploadFile = require('../middleware/upload');
+const uploadFile = require("../middleware/upload");
+const fs = require("fs");
+const baseUrl = "http://localhost:8080/files/";
 
 const upload = async (req, res) => {
   try {
